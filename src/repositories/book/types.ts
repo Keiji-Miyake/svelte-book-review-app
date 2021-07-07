59322b319ff6b29faf2bcf9efd11f0da0928fd58
Keiji-Miyake <k.miyake01+dev@gmail.com>
2021-07-07 11:25:22 +0900

    ブックリポジトリの作成

---
+++ src/repositories/book/types.ts
@@ -0,0 +1,46 @@
+/**
+ * Google Books API のレスポンス
+ */
+export interface Result {
+    items: BookItem[];
+    kind: string;
+    totalItems: number;
+}
+
+/**
+ * 本の情報
+ */
+export interface BookItem {
+    id: string;
+    valumeInfo: {
+        title: string;
+        authors: string[];
+        publishedDate?: string;
+        description?: string;
+        publisher?: string;
+        imageLinks?: {
+            smallThumbnail: string;
+            thumbnail: string;
+        }
+        pageCount: number;
+        previewLink?: string;
+    };
+    saleInfo?: {
+        listPrice: {
+            amount: number;
+        };
+    };
+}
+
+/**
+ * query parameters
+ */
+export interface Params {
+    q: string;
+    startIndex?: number;
+}
+
+export interface BookRepositoryInterface {
+    get(params: Params): Promise<Result>;
+    find(id: string): Promise<BookItem>;
+}
