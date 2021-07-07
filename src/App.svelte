645ea744d4dabf206926a8a07d8a5a147225b443
Keiji-Miyake <k.miyake01+dev@gmail.com>
2021-07-07 10:40:51 +0900

    ルーティングの設定

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import Tailwindcss from './Tailwindcss.svelte';
-	import Header from './components/Header.svelte'
-
-	export let name: string;
+	import Header from './components/Header.svelte';
+	import Router from 'svelte-spa-router';
+	import { routes } from './router'
 </script>
 
 <Tailwindcss />
 <Header />
 <main class="pt-16">
 	<div class="container mx-auto px-6 my-4">
-		<h1>Hello {name}!</h1>
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+		<Router {routes} />
 	</div>
 </main>
