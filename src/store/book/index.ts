c64acad4b125c53582106b9506ba56a32e172517
Keiji-Miyake <k.miyake01+dev@gmail.com>
2021-07-07 14:15:51 +0900

    ストアの実装

---
+++ src/store/book/index.ts
@@ -0,0 +1,17 @@
+import { writable } from "svelte/store";
+import type { BookItem } from '../../repositories/book'
+
+const useBookStore = () => {
+    const { subscribe, set, update } = writable<BookItem[]>([])
+    const reset = () => set([])
+    const add = (newBooks: BookItem[]) => update((books: BookItem[]) => {
+        return {...books, ...newBooks}
+    })
+    return {
+        subscribe,
+        reset,
+        add
+    }
+}
+
+export const books = useBookStore()
